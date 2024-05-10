0731ffdc1184b03ddcdb5e08f993b614fb1e16a7
Ne-Ga-TIV <anton.antonio.10@mail.ru>
2024-05-10 18:39:34 +0300

    Update GetProjects method(Project end-point)

--- Event-planning-back.DataAccess/Repositories/UserRepository.cs
+++ Event-planning-back.DataAccess/Repositories/UserRepository.cs
@@ -41,20 +41,16 @@ public class UserRepository : IUserRepository
 
     public async Task<User?> GetByIdWithProjects(Guid id)
     {
-        var userEntity = await _context.Users
-            .Include(u => u.Projects)
-            .FirstOrDefaultAsync(u => u.Id == id);
-        if (userEntity == null)
+        var userEntity = await _context.Users.FindAsync(id);
+        var projects = await GetProjects(id);
+
+        if (userEntity == null || projects == null)
         {
             return null;
         }
+        
         return User.Create(userEntity.Id, userEntity.UserName, userEntity.UserSurname, userEntity.PasswordHash,
-            userEntity.Email, userEntity.Projects.
-                Select(p => 
-                    Project.Create(p.Id, 
-                        p.ProjectName, 
-                        p.Description))
-                .ToList()).User;
+            userEntity.Email, projects).User;
     }
     public async Task<Guid> Create(User user)
     {
@@ -111,8 +107,10 @@ public class UserRepository : IUserRepository
         var userEntity = await _context.Users
             .Include(u => u.Projects)
             .FirstOrDefaultAsync(u => u.Id == userId);
+        
         if (userEntity == null)
             return null;
+        
         var projects = userEntity.Projects.Select(p => Project.Create(
             p.Id,
             p.ProjectName,
